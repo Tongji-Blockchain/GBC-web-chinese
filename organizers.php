e497083fc7b8bcb8d1787a74713375e66c7569df
Zihan Zhou <786187157@qq.com>
2023-12-12 22:50:51 +0800

    从英文版copy

---
+++ organizers.php
@@ -0,0 +1,96 @@
+<?php 
+$pageTitle='Organizers'; //THIS MUST BE SET FOR EACH PAGE
+$stylesheet='css/organizers.css';
+require 'header-left-bar.php'; //THIS MUST BE INCLUDED FOR EACH PAGE
+?>
+
+<!-- <div class="text">
+	This is where the organizers go.
+</div> -->
+
+<div style="margin-top:5%; margin-bottom: 5%" class="flex-column">
+
+	<!-- <div class="organizer-group-title">Chair</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/liuerwu.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">刘儿兀</div>
+			<div class="role">IET Fellow</div>
+			<div class="affiliation">Tongji University, China</div>
+		</div>
+	</div>
+
+	<div class="spacer"></div> -->
+
+	<div class="organizer-group-title">TPC Co-Chairs</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/elisa.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Elisa Bertino</div>
+			<div class="role bold">IEEE Fellow, ACM Fellow</div>
+			<div class="affiliation">Purdue University, USA</div>
+		</div>
+	</div>
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/chonggangwang.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Chonggang Wang</div>
+			<div class="role bold">IEEE Fellow</div>
+			<div class="affiliation">InterDigital, Inc., USA</div>
+		</div>
+	</div>
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/cuishuguang.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">崔曙光</div>
+			<div class="role bold">IEEE Fellow</div>
+			<div class="affiliation">The Chinese University of Hong Kong, Shenzhen, China</div>
+		</div>
+	</div>
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/renkui.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">任奎</div>
+			<div class="role bold">IEEE Fellow, ACM Fellow</div>
+			<div class="affiliation">Zhejiang University, China</div>
+		</div>
+	</div>
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/leizhang.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Lei Zhang</div>
+			<div class="role bold">Associate Editor of IEEE Internet of Things Journal</div>
+			<div class="affiliation">University of Glasgow, UK</div>
+		</div>
+	</div>
+	
+	<div class="spacer"></div>
+	
+	<div class="organizer-group-title">KeyNote Speaker</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/thomas.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Thomas Coughlin</div>
+			<div class="role">IEEE President Elect</div>
+			<!-- <div class="affiliation"></div> -->
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/elisa.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Elisa Bertino</div>
+			<div class="role">ACM Vice President</div>
+			<!-- <div class="affiliation"></div> -->
+		</div>
+	</div>
+	
+
+	<div class="spacer"></div>
+
+</div>
+
+<?php require 'right-bar-footer.php'; //THE MUST BE INCLUDED FOR EACH PAGE ?>
